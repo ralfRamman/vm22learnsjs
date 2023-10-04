cb4b46eecce84be6ebaf408ebb4a6bf2e49f12e3
Ralf Rämman <Ralf.ramman@tptlive.ee>
2023-10-04 14:09:53 +0300

    array

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-console.log('hello world')
+array[3] = 96;
+console.log(array);
+array.push(`new value`)
+console.log(array);
+array = [
 
-let num; 1;
+    [1,2,3]
+    [4,5,6]
+    [7,8,9]
 
+];
+console.log(array);
+console.log(array[1][2]);
+array = [[[[[1]]]]]
+console.log(array[0][0][0][0][0]);
 
-console.log(num);
-
-let sting = 'hello';
-string = `hello
-im multi line 
-string`;
-
+let object = {
+    name: `ralf`
+    age: 18,
+    dog: [`umberto`]
+}
+console.log(object)
